377c304adcd6cf8b7628b24b10d39c7c24226779
Igor Pavloski <igor.pavloski@seavus.com>
2018-06-12 17:22:08 +0200

    do 39 ae

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Igor', age: 30 },
+      { name: 'Joe', age: 84 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <p>This is workings</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
+
+export default App;
